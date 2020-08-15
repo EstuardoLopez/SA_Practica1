cde10512d500b5ff9a18f95113a6831eab1a10f6
Estuardo Lopez <erlopez@bdgsa.net>
2020-08-14 22:42:41 -0600

    Elopez: Auth basic

--- ClientSoap/Client/ClientSOAP.cs
+++ ClientSoap/Client/ClientSOAP.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,11 +11,15 @@ namespace ClientSoap.Client
 {
     public class ClientSOAP
     {
-        SoftwareAvanzado.administratorcontact100Client client = null;
+        SoftwareAvanzadoAuth.administratorcontact100Client client = null;
+        
+        //private string ClientSecret = null;
 
         public ClientSOAP()
         {
-            client = new SoftwareAvanzado.administratorcontact100Client();
+            client = new SoftwareAvanzadoAuth.administratorcontact100Client();
+            client.ClientCredentials.UserName.UserName = "sa";
+            client.ClientCredentials.UserName.Password = "usac";
         }
 
         /// <summary>
@@ -23,20 +30,31 @@ namespace ClientSoap.Client
         public List<ContactoModel> GetContacts(ContactoModel filter)
         {
             List<ContactoModel> result = new List<ContactoModel>();
-            SoftwareAvanzado.readListResponse_list_item[] resultWS = null;            
-            resultWS = client.readList(0, 50, filter.Nombre,null,null,null,null);
-
-            if (resultWS != null && resultWS.Length >= 0)
+            using (new OperationContextScope(client.InnerChannel))
             {
-                for(int i=0;  i < resultWS.Length; i++)
+                // Add a HTTP Header to an outgoing request
+                string auth = "Basic " + Convert.ToBase64String(System.Text.Encoding.Default.GetBytes(client.ClientCredentials.UserName.UserName + ":" + client.ClientCredentials.UserName.Password));
+                HttpRequestMessageProperty requestMessage = new HttpRequestMessageProperty();
+                requestMessage.Headers["Authorization"] = auth;
+                OperationContext.Current.OutgoingMessageProperties[HttpRequestMessageProperty.Name] = requestMessage;
+               // client.GetCampaignIds(out campaignRecords);
+
+                SoftwareAvanzadoAuth.readListResponse_list_item[] resultWS = null;
+                resultWS = client.readList(0, 50, filter.Nombre, null, null, null, null);
+
+                if (resultWS != null && resultWS.Length >= 0)
                 {
-                    result.Add(new ContactoModel()
+                    for (int i = 0; i < resultWS.Length; i++)
                     {
-                        Id = resultWS[i].id,
-                        Nombre = resultWS[i].name
-                    });
+                        result.Add(new ContactoModel()
+                        {
+                            Id = resultWS[i].id,
+                            Nombre = resultWS[i].name
+                        });
+                    }
                 }
             }
+            
             return result;
         }
 
@@ -56,8 +74,18 @@ namespace ClientSoap.Client
             int CantidadEnviados = 0;
             for(int i=0; i<data.Cantidad; i++)
             {
-                result.status = client.create(data.Nombre + "__" + i, 0, "", 0);
-                if(result.status) CantidadEnviados++;
+                using (new OperationContextScope(client.InnerChannel))
+                {
+                    string auth = "Basic " + Convert.ToBase64String(System.Text.Encoding.Default.GetBytes(client.ClientCredentials.UserName.UserName + ":" + client.ClientCredentials.UserName.Password));
+                    HttpRequestMessageProperty requestMessage = new HttpRequestMessageProperty();
+                    requestMessage.Headers["Authorization"] = auth;
+                    OperationContext.Current.OutgoingMessageProperties[HttpRequestMessageProperty.Name] = requestMessage;
+                    // client.GetCampaignIds(out campaignRecords);
+
+                    SoftwareAvanzadoAuth.readListResponse_list_item[] resultWS = null;
+                    result.status = client.create(data.Nombre + "__" + i, 0, "", 0);
+                    if (result.status) CantidadEnviados++;
+                }
             }
 
             if (data.Cantidad == CantidadEnviados)
---
+++ ClientSoap/Parte1/TokenGenerator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Web;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ClientSoap.Parte1
+{
+    public class TokenGenerator
+    {
+        public string WebServiceBaseUrl { get; set; }
+        private string Tuser { get; set; }
+        private string Tpass { get; set; }
+        byte[] byteArray { get; set; }
+
+
+        public TokenGenerator()
+        {
+            this.WebServiceBaseUrl = "";
+            this.Tuser = "sa";
+            this.Tpass = "usac";
+            this.byteArray = new UTF8Encoding().GetBytes(Tuser + ":" + Tpass);
+        }
+
+        public async Task<Tout> PostWS<Tin, Tout>(string urlMetodo, Tin datosPorEnviar)
+        {
+            Tout respuesta = default(Tout);
+            //using (var client = new HttpClient())
+            //{
+
+            //    client.DefaultRequestHeaders.Accept.Clear();
+            //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+            //    HttpResponseMessage response;
+            //    try
+            //    {
+
+            //        response = await client.PostAsync(WebServiceBaseUrl + urlMetodo);
+            //        if (response.IsSuccessStatusCode)
+            //        {
+            //            respuesta = await response.Content.ReadAsAsync<Tout>();
+            //        }
+            //        else
+            //        {
+            //            var error = await response.Content.ReadAsStringAsync();
+            //            throw new HttpRequestException(error);
+            //        }
+
+            //        var a = client
+            //    }
+            //    catch (HttpRequestException e)
+            //    {
+            //        // Handle exception.                    
+
+            //        throw;
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        // Handle exception.
+
+            //        throw;
+            //    }
+            //}
+
+            return respuesta;
+        }
+
+    }
+}
